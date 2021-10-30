bbae8d0161389e9f2f3e4c6103bf8175f2f8b7e4
abdouDev <trigui.abdou95@gmail.com>
2021-10-30 13:43:35 +0200

    Import lib into src project and edit ng-ab-tel-input selector name

--- projects/ng-ab-tel-input/src/lib/ng-ab-tel-input.component.ts
+++ projects/ng-ab-tel-input/src/lib/ng-ab-tel-input.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'lib-ng-ab-tel-input',
+  selector: 'ng-ab-tel-input',
   template: `
     <p>
       ng-ab-tel-input works!
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgAbTelInputModule } from 'projects/ng-ab-tel-input/src/lib/ng-ab-tel-input.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgAbTelInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
